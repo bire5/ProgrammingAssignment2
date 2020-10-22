a114312e0393358a429cab6129023034bb7ea499
bire5 <lifayamga@gmail.com>
2020-10-22 03:10:00 +0000

    Week 3 peer-graded assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These are two functions that create a special matrix and caches the
+## inverse of the matrix to reduce the time spent in
 
-## Write a short comment describing this function
+## This function creates a special matrix that caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ma <- NULL
+  set <- function(y) {
+    x <<- y
+    ma <<- NULL
+  }
+  get <- function()
+    x
+  setInv <- function(invMatrix)
+    ma <<- invMatrix
+  getInv <- function()
+    ma
+  list(
+    set = set,
+    get = get,
+    setInv = setInv,
+    getInv = getInv
+  )
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ma <- x$getInv()
+  if (!is.null(ma)) {
+    message("getting cached data")
+    return(ma)
+  }
+  data <- x$get()
+  ma <- ans(data)
+  x$setInv(ma)
+  ma
+  
 }
